2c9e765486e5b141318dc6ba19c1c942b9d4543d
gaving-quadprep <148482416+gaving-quadprep@users.noreply.github.com>
2023-10-21 21:19:23 -0400

    Add files via upload

--- towerGame/src/towerGame/map/Level.java
+++ towerGame/src/towerGame/map/Level.java
@@ -16,7 +16,7 @@ public class Level {
 		tiles=new Tile[size][size];
 		for(int x=0;x<size;x++) {
 			for(int y=0;y<size;y++) {
-				tiles[x][y]=new Tile(this,y==5?1:0,x,y);
+				tiles[x][y]=new Tile(this,y>8?5:y>6&y<9&x==7?6:y>2&x>4&x<10?(3):y==2&x>4&x<10&(1&x)==1?3:0,x,y); // temporary
 			}
 		}
     	try {
