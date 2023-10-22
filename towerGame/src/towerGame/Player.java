203cee2fe7fadd5fba977d0241d30338ec329328
gaving-quadprep <148482416+gaving-quadprep@users.noreply.github.com>
2023-10-22 19:14:22 -0400

    Add files via upload

---
+++ towerGame/src/towerGame/Player.java
@@ -0,0 +1,43 @@
+package towerGame;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Player extends Entity {
+	public float maxHealth=10.0F;
+	public float health=maxHealth;
+	public float xVelocity;
+	public float yVelocity;
+	public Player() {
+		posX=320;
+		posY=240;
+	}
+	public void loadImages() {
+    	try {
+    		this.sprite=ImageIO.read(getClass().getResourceAsStream("/player.png"));
+    	}catch(IOException e) {
+    		e.printStackTrace();
+    		System.out.println("Player sprite failed to load!");
+    	}
+	}
+	@Override
+	public void update() {
+		this.update((KeyHandler)null);
+	}
+	public void update(KeyHandler keyHandler) {
+		if(keyHandler.upPressed) {this.posY--;this.yVelocity=-2;};
+		if(keyHandler.downPressed) {this.posY++;};
+		if(keyHandler.leftPressed) {this.posX--;};
+		if(keyHandler.rightPressed) {this.posX++;};
+		this.posX+=xVelocity;
+		this.yVelocity+=0.087F;//gravity
+		this.posY+=yVelocity;
+	}
+	@Override
+	public void render(Graphics2D g2) {
+		g2.drawImage(this.sprite,(int)this.posX,(int)this.posY,32,32,null);
+	}
+}
---
+++ towerGame/src/towerGame/map/Tile.java
@@ -0,0 +1,37 @@
+package towerGame.map;
+
+import java.awt.Graphics2D;
+import java.awt.image.ImageObserver;
+
+public class Tile {
+	public final int id;
+	public final boolean isSolid;
+	public static final Tile[] tiles=new Tile[256];
+	public Tile(int id, boolean isSolid) {
+		tiles[id]=this;
+		this.id=id;	
+		this.isSolid=isSolid;
+		//this.level=level;
+	}
+	public int getTextureId() {
+		return this.id;
+	}
+	public void update(Level level, int posX, int posY) {};
+	public void render(Level level, Graphics2D g2, int posX, int posY) {
+		
+		int frameX = (this.id % 16) * 16;
+		int frameY = (this.id / 16) * 16;
+		g2.drawImage(level.tilemap, posX*32, posY*32, posX*32+32, posY*32+32,
+	    frameX, frameY, frameX+16, frameY+16, (ImageObserver)null);
+
+	};
+	public static Tile air=new Tile(0,false);
+	public static Tile stone=new Tile(1,true);
+	public static Tile crackedStone=new Tile(2,true);
+	public static Tile bricks=new Tile(3,true);
+	public static Tile crackedBricks=new Tile(4,true);
+	public static Tile grass=new Tile(5,true);
+	public static Tile wood=new Tile(6,true);
+	public static Tile tallGrass=new Tile(7,false);
+	
+}
