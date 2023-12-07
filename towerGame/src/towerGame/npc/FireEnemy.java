2dafa034d939bb70c2080bf3d4a0d2e7746abeac
gaving-quadprep <148482416+gaving-quadprep@users.noreply.github.com>
2023-12-07 13:32:00 -0500

    Add files via upload

--- towerGame/src/towerGame/npc/FireEnemy.java
+++ towerGame/src/towerGame/npc/FireEnemy.java
@@ -3,16 +3,19 @@ package towerGame.npc;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
+import java.awt.Rectangle;
 
+import towerGame.Entity;
+import towerGame.Player;
 import towerGame.map.Level;
 
 public class FireEnemy extends Enemy {
 	public boolean isBlue;
 	public int baseY;
-	
 	public FireEnemy(Level level, boolean isBlue) {
 		super(level);
 		this.isBlue=isBlue;
+		this.hitbox=new Rectangle(0,0,16,16);
 	}
 	public FireEnemy(Level level) {
 		this(level,false);
@@ -20,6 +23,13 @@ public class FireEnemy extends Enemy {
 	@Override
 	public void update() {
 		this.posY=baseY+(float) Math.sin(System.nanoTime()/500000000.0D);
+		for(Entity e : this.level.entities) {
+			if(e instanceof Player) {
+				if(this.level.cc.checkAABB(this.posX+this.hitbox.x, this.posY+this.hitbox.y, this.posX+this.hitbox.x+this.hitbox.width, this.posY+this.hitbox.y+this.hitbox.height, e.posX+e.hitbox.x, e.posY+e.hitbox.y, e.posX+e.hitbox.x+e.hitbox.width, e.posY+e.hitbox.y+e.hitbox.height)) {
+					JOptionPane.showMessageDialog(null, "q", "q", JOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+		}
 	}
 	@Override
 	public void loadImages() {
