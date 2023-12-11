d761bf963327a02c70aba38a4c44f56f5afc0113
gaving-quadprep <148482416+gaving-quadprep@users.noreply.github.com>
2023-12-11 11:44:23 -0500

    Add files via upload

--- towerGame/src/towerGame/npc/Thing.java
+++ towerGame/src/towerGame/npc/Thing.java
@@ -1,10 +1,22 @@
 package towerGame.npc;
 
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+
 import towerGame.map.Level;
 
 public class Thing extends Enemy {
 
 	public Thing(Level level) {
 		super(level);
+	}
+	@Override
+	public void loadImages() {
+
+		try {
+			this.sprite=ImageIO.read(getClass().getResourceAsStream("/thing.png"));
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Failed to load sprite", "Error", JOptionPane.ERROR_MESSAGE);
+		}
 	}
 }
